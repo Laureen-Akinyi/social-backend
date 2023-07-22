c85618fe680c7699287b26618b6dc491a3fd98f1
Laureen Akinyi <loraneakinyi@gmail.com>
2023-07-22 17:36:56 +0300

    updates the routes and config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get "posts", to: 'posts#index'
+  get "posts/:id", to: 'posts#show'
+
   # root "articles#index"
+  resources :users, only: [:index, :show, :create]
+
+  #Authentication routes
+  post '/register', to: "users#create_user"
+
+  post '/login', to: "users#login_user"
+
+  # get '/me', to: "users#login_user"
+
+  delete '/logout', to: "users#logout_user"
 end
