bac333172adfb05d590c394019fef1ec92af7d6c
Laureen Akinyi <loraneakinyi@gmail.com>
2023-07-22 17:52:12 +0300

    updates the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :followers
+    validates :email, uniqueness: true, presence: true, on: :account_setup
+    validates :user_name, uniqueness: true, presence: true
 end
