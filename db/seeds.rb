74d5b1616a9c029f9c72dba54881647c825d5b8e
Laureen Akinyi <loraneakinyi@gmail.com>
2023-07-22 17:56:36 +0300

    updating the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "seeding users"
+    User.create({
+        user_name: "Laureen Akinyi",
+        email: "laureen.akinyi@gmail.com",
+        password: "123",
+    })
+    User.create({
+        user_name: "Reginal Spector",
+        email: "regi.spector@gmail.com",
+        password: "123",
+    })
+puts "done seeding users"
